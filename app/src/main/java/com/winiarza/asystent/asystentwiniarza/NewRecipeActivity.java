0443abe00fa0afe6973c027e3ddbefdabec9c1a4
Ehudaszek <emhudaszek@gmail.com>
2018-12-03 15:17:00 +0100

    Activity

---
+++ app/src/main/java/com/winiarza/asystent/asystentwiniarza/NewRecipeActivity.java
@@ -0,0 +1,13 @@
+package com.winiarza.asystent.asystentwiniarza.Models;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_recipe);
+    }
+}
