b12d06d64765648a2b905cd69dfaa0421430aa62
michal <michalkabionek@gmail.com>
2018-12-10 18:27:10 +0100

    Add reminder

--- app/src/main/java/com/winiarza/asystent/asystentwiniarza/MainActivity.java
+++ app/src/main/java/com/winiarza/asystent/asystentwiniarza/MainActivity.java
@@ -3,6 +3,7 @@ package com.winiarza.asystent.asystentwiniarza;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.provider.CalendarContract;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
--- app/src/main/java/com/winiarza/asystent/asystentwiniarza/RemindersActivity.java
+++ app/src/main/java/com/winiarza/asystent/asystentwiniarza/RemindersActivity.java
@@ -1,13 +1,43 @@
 package com.winiarza.asystent.asystentwiniarza;
 
+import android.content.Intent;
+import android.provider.CalendarContract.Events;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Calendar;
 
 public class RemindersActivity extends AppCompatActivity {
+
+    Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+
+        addBtn = findViewById(R.id.addReminderBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar beginTime = Calendar.getInstance();
+                beginTime.set(2018, 12, 11, 15, 00);
+                Calendar endTime = Calendar.getInstance();
+                endTime.set(2018, 12, 11, 23, 00);
+
+                Intent intent = new Intent(Intent.ACTION_INSERT);
+                intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+                intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+                intent.putExtra(Events.TITLE, "Nalewka");
+                intent.putExtra(Events.DESCRIPTION, "Zasypać cukrem");
+                intent.setType("vnd.android.cursor.item/event");
+                intent.putExtra("allDay", true);
+
+                startActivity(intent);
+            }
+        });
     }
 }
